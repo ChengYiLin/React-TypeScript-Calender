5f7911dc0267611777bf3024f2bc6cf1e11a99e9
Cheng Yi Lin <cheng.yi.lin.217@gmail.com>
2021-09-29 02:58:12 +0800

    First Commit : Build React, TypeScript and Webpack Template

---
+++ src/index.tsx
@@ -0,0 +1,11 @@
+import React from "react"
+import ReactDOM from "react-dom";
+
+const App = () => (
+  <h1>Hello, React, TypeScripts and Webpack</h1>
+);
+
+ReactDOM.render(
+  <App />,
+  document.getElementById("root")
+);
---
+++ webpack.config.ts
@@ -0,0 +1,54 @@
+import path from "path";
+import { Configuration as WebpackConfiguration } from "webpack";
+import { Configuration as WebpackDevServerConfiguration } from "webpack-dev-server";
+import ForkTsCheckerWebpackPlugin from 'fork-ts-checker-webpack-plugin';
+
+
+interface Configuration extends WebpackConfiguration {
+  devServer?: WebpackDevServerConfiguration;
+}
+
+const config: Configuration = {
+  entry: "./src/index.tsx",
+  resolve: {
+    extensions: [".tsx", ".ts", ".js"],
+  },
+  output: {
+    path: path.resolve(__dirname, "build"),
+    filename: "bundle.js",
+  },
+  devServer: {
+    static: path.join(__dirname, "build"),
+    compress: true,
+    port: 4000,
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(ts|js)x?$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader",
+          options: {
+            presets: [
+              "@babel/preset-env",
+              "@babel/preset-react",
+              "@babel/preset-typescript",
+            ],
+          },
+        },
+      },
+    ],
+  },
+  plugins: [
+    new ForkTsCheckerWebpackPlugin({
+      async: false,
+      eslint: {
+        files: "./src/**/*",
+      },
+    }),
+  ],
+
+};
+
+export default config;
