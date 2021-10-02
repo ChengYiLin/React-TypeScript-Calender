ab2397b8eabba87158b56e735ccd3966227b8112
Cheng Yi Lin <cheng.yi.lin.217@gmail.com>
2021-10-03 03:19:13 +0800

    feat: set `viewTypeSwitch` describtion text

--- src/Calender/components/viewTypeSwitch.tsx
+++ src/Calender/components/viewTypeSwitch.tsx
@@ -3,6 +3,7 @@ import styled from 'styled-components'
 import dayjs from 'dayjs'
 // Context
 import { CalenderContext } from '../context'
+import { viewTypes } from '../types/calender'
 
 const Container = styled.button`
     width: 200px;
@@ -21,14 +22,38 @@ const Content = styled.div`
     padding: 5px;
 `
 
+interface DescTxtProps {
+    viewType: viewTypes;
+    currentDate: Date;
+    
+}
+
+const ViewTypeDescTxt: FC<DescTxtProps> = (props) => {
+	const {viewType, currentDate} = props
+
+	const startYear = Math.floor(dayjs(currentDate).year() / 10) * 10
+
+	switch (viewType) {
+	case 'year':
+		return <span>{`${startYear} - ${startYear + 9}`}</span>
+    
+	case 'month':
+		return <span>{dayjs(currentDate).year()}</span>
+
+	default:
+		return <span>{`${dayjs(currentDate).month()} ${dayjs(currentDate).year()}`}</span>
+	}
+}
+
 const ViewTypeSwitch: FC = () => {
-	const { viewType, handleChangeViewType } = useContext(CalenderContext)
+	const { viewType, currentDate, handleChangeViewType } = useContext(CalenderContext)
 
 	return (
 		<Container onClick={() => handleChangeViewType()}>
 			<Content>
-				<span>{viewType} </span>
-				{dayjs().year()}
+				<ViewTypeDescTxt
+					viewType={viewType}
+					currentDate={currentDate}/>
 			</Content>
 		</Container>
 	)
