b7d15eac292de77dd248cd6e2df033677d677c79
Cheng Yi Lin <cheng.yi.lin.217@gmail.com>
2021-10-02 14:44:16 +0800

    feat : create `type` describtion file  and `enum` file

---
+++ src/Calender/types/enum.ts
@@ -0,0 +1,24 @@
+export enum DayOfWeekEnum {
+    'Su',
+    'Mo',
+    'Tu',
+    'We',
+    'Th',
+    'Fr',
+    'Sa'
+}
+
+export enum MonthEnum {
+    'Jan',
+    'Feb',
+    'Mar',
+    'Apr',
+    'May',
+    'Jun',
+    'Jul',
+    'Aug',
+    'Sep',
+    'Oct',
+    'Nov',
+    'Dec'
+}
