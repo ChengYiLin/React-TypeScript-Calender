425dd1d6bbec67cc2fe700136eaae6517aba2915
Cheng Yi Lin <cheng.yi.lin.217@gmail.com>
2021-10-03 01:26:33 +0800

    style : lint for `webpack.config` and `tsconfig`

--- webpack.config.ts
+++ webpack.config.ts
@@ -1,7 +1,7 @@
-import path from "path";
-import { Configuration as WebpackConfiguration } from "webpack";
-import { Configuration as WebpackDevServerConfiguration } from "webpack-dev-server";
-import ForkTsCheckerWebpackPlugin from 'fork-ts-checker-webpack-plugin';
+import path from 'path'
+import { Configuration as WebpackConfiguration } from 'webpack'
+import { Configuration as WebpackDevServerConfiguration } from 'webpack-dev-server'
+import ForkTsCheckerWebpackPlugin from 'fork-ts-checker-webpack-plugin'
 
 
 interface Configuration extends WebpackConfiguration {
@@ -9,46 +9,45 @@ interface Configuration extends WebpackConfiguration {
 }
 
 const config: Configuration = {
-  entry: "./src/index.tsx",
-  resolve: {
-    extensions: [".tsx", ".ts", ".js"],
-  },
-  output: {
-    path: path.resolve(__dirname, "build"),
-    filename: "bundle.js",
-  },
-  devServer: {
-    static: path.join(__dirname, "build"),
-    compress: true,
-    port: 4000,
-  },
-  module: {
-    rules: [
-      {
-        test: /\.(ts|js)x?$/,
-        exclude: /node_modules/,
-        use: {
-          loader: "babel-loader",
-          options: {
-            presets: [
-              "@babel/preset-env",
-              "@babel/preset-react",
-              "@babel/preset-typescript",
-            ],
-          },
-        },
-      },
-    ],
-  },
-  plugins: [
-    new ForkTsCheckerWebpackPlugin({
-      async: false,
-      eslint: {
-        files: "./src/**/*",
-      },
-    }),
-  ],
+	entry: './src/index.tsx',
+	resolve: {
+		extensions: ['.tsx', '.ts', '.js'],
+	},
+	output: {
+		path: path.resolve(__dirname, 'build'),
+		filename: 'bundle.js',
+	},
+	devServer: {
+		static: path.join(__dirname, 'build'),
+		compress: true,
+		port: 4000,
+	},
+	module: {
+		rules: [
+			{
+				test: /\.(ts|js)x?$/,
+				exclude: /node_modules/,
+				use: {
+					loader: 'babel-loader',
+					options: {
+						presets: [
+							'@babel/preset-env',
+							'@babel/preset-react',
+							'@babel/preset-typescript',
+						],
+					},
+				},
+			},
+		],
+	},
+	plugins: [
+		new ForkTsCheckerWebpackPlugin({
+			async: false,
+			eslint: {
+				files: './src/**/*',
+			},
+		}),
+	]
+}
 
-};
-
-export default config;
+export default config
